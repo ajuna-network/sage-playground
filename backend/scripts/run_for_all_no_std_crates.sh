b1d062c74d051cb36bc3d93ceb37d60077d3794c
Didac Semente Fernandez <didac@ajuna.io>
2025-03-05 16:54:11 +0100

    * Adjusted features * Adjusted check WASM script

--- backend/scripts/run_for_all_no_std_crates.sh
+++ backend/scripts/run_for_all_no_std_crates.sh
@@ -15,6 +15,11 @@ find . -name "Cargo.toml" | while read -r CARGO_TOML; do
   DIR=$(dirname "$CARGO_TOML")
   echo "Checking in directory: $DIR"
 
+  # Skip the node crate since it's not meant to be built in WASM
+  if [ "$DIR" = "./node" ]; then
+    continue
+  fi
+
   # Skip the loop if the crate does not have a feature `std`
   if ! grep -q "\[features\]" "$CARGO_TOML" || ! grep -q "std = \[" "$CARGO_TOML"; then
       echo "Feature 'std' not found in $CARGO_TOML. Skipping."
