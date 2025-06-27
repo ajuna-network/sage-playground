e9ccd9156ffd34819b901f6e004756d90c823ce1
Ed Anderson <36516131+eca20@users.noreply.github.com>
2025-06-27 15:12:54 -0400

    SAGE.dll integrated, project compiles

---
+++ Assets/Scripts/HelloSage.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System;
+using System.Reflection;
+
+public class HelloSage : MonoBehaviour
+{
+    void Start()
+    {
+        Assembly sageCoreAssembly = typeof(Ajuna.SAGE.Core.Utils).Assembly;
+        Version sageVersion = sageCoreAssembly.GetName().Version;
+        Debug.Log($"Ajuna.SAGE.Core version: {sageVersion}");
+    }
+}
